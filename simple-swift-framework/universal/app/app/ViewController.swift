ee1a4bcf9c80619809360c9a824439b27b2da63f
quang.ha <quangDecember@gmail.com>
2018-12-16 23:19:41 +0800

    Simple project to prove that things work

---
+++ simple-swift-framework/universal/app/app/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  app
+//
+//  Created by Quang on 12/16/18.
+//  Copyright © 2018 Quang. All rights reserved.
+//
+
+import UIKit
+import universal
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let _ = MathOperator.add(0, 1)
+    }
+
+
+}
+
