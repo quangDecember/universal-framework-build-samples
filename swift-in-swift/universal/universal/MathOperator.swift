3b2162243bdabb4bc90000a1f3ce77e0287afe10
quang.ha <quangDecember@gmail.com>
2018-12-16 18:30:17 +0800

    Setup first project

---
+++ swift-in-swift/universal/universal/MathOperator.swift
@@ -0,0 +1,15 @@
+//
+//  MathOperator.swift
+//  universal
+//
+//  Created by Quang on 12/16/18.
+//  Copyright © 2018 Quang. All rights reserved.
+//
+
+import Foundation
+
+public struct MathOperator {
+    public static func add(_ x: Int, _ y : Int)  -> Int {
+        return x + y
+    }
+}
---
+++ swift-in-swift/universal/universal/universal.h
@@ -0,0 +1,19 @@
+//
+//  universal.h
+//  universal
+//
+//  Created by Quang on 12/16/18.
+//  Copyright © 2018 Quang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for universal.
+FOUNDATION_EXPORT double universalVersionNumber;
+
+//! Project version string for universal.
+FOUNDATION_EXPORT const unsigned char universalVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <universal/PublicHeader.h>
+
+
